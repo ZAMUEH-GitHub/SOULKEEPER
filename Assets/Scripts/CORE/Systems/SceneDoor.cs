d9e12ed915877da3d0239b546a5d41f7875bb266
ZAMUEH <samuelnavarroginer@hotmail.com>
2025-10-16 21:18:23 +0200

    CODE - Initial Setup for Scene Manager Scripts

---
+++ Assets/Scripts/CORE/Systems/SceneDoor.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class SceneDoor : MonoBehaviour, IInteractable
+{
+    [Header("Load Scene")]
+    [SerializeField] private SceneField loadScene;
+
+    public void Interact()
+    {
+
+    }
+}
