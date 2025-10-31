d2498ce72a730c0d4b6ad5cee5f7d640825f14ca
ZAMUEH <samuelnavarroginer@hotmail.com>
2025-10-31 14:05:08 +0100

    CODE - Subtle Corrections for Altar System Initial Setup
    - Modified "AltarController.cs" for simpler code
    - Modified "TEST_Altars.unity" for current corrections

--- Assets/Scripts/PROPS/ALTARS/AltarController.cs
+++ Assets/Scripts/PROPS/ALTARS/AltarController.cs
@@ -4,12 +4,6 @@ public class AltarController : MonoBehaviour, IAltar, IInteractable
 {
     [Header("Altar Setup")]
     [SerializeField] private PowerUpDefinition powerUp;
-    [SerializeField] private bool oneShot = true;
-
-    [Header("Feedback (optional)")]
-    [SerializeField] private ParticleSystem useVFX;
-    [SerializeField] private AudioSource useSFX;
-    [SerializeField] private GameObject disableOnUse;
 
     private bool used;
     private PlayerPowerUpController player;
@@ -20,7 +14,6 @@ public class AltarController : MonoBehaviour, IAltar, IInteractable
     {
         var playerObj = GameObject.FindGameObjectWithTag("Player");
         if (playerObj != null) player = playerObj.GetComponent<PlayerPowerUpController>();
-
     }
 
     public void Interact()
@@ -30,8 +23,6 @@ public class AltarController : MonoBehaviour, IAltar, IInteractable
 
     public void UnlockPowerUp()
     {
-        if (used && oneShot) return;
-
         var target = player;
         if (target == null)
         {
@@ -40,22 +31,5 @@ public class AltarController : MonoBehaviour, IAltar, IInteractable
         }
 
         target.UnlockPowerUp(powerUp);
-
-        if (oneShot) used = true;
-
-        if (useVFX != null)
-            Instantiate(useVFX, transform.position, Quaternion.identity);
-
-        if (useSFX != null)
-            useSFX.Play();
-
-        if (disableOnUse != null && oneShot)
-            disableOnUse.SetActive(false);
-
-        if (oneShot)
-        {
-            var col = GetComponent<Collider2D>();
-            if (col != null) col.enabled = false;
-        }
     }
 }
