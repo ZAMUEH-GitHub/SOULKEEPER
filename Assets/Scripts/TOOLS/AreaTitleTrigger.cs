038d89cfacf28c6653c32db21562a3e3ea045d1e
ZAMUEH <samuelnavarroginer@hotmail.com>
2025-11-20 00:35:52 +0100

    CODE - Area Title Fix
    - Modified "AreaTitleManager.cs" for Player spawn fix

--- Assets/Scripts/TOOLS/AreaTitleTrigger.cs
+++ Assets/Scripts/TOOLS/AreaTitleTrigger.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]
@@ -8,6 +9,18 @@ public class AreaTitleTrigger : MonoBehaviour
     [SerializeField] private string titleMessage;
     [SerializeField] private string subtitleMessage;
     [SerializeField] private float messageDuration;
+
+    private Collider2D cL;
+
+    private IEnumerator Start()
+    {
+        cL = GetComponent<Collider2D>();
+        cL.enabled = false;
+
+        yield return new WaitForSeconds(0.5f);
+
+        cL.enabled = true;
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
