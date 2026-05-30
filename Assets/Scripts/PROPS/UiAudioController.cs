2765e19571aa2386831b726218ffb5efd6ecdd93
Mika <arimailunak@gmail.com>
2026-05-30 03:17:55 +0200

    CODE - UiSoundController

---
+++ Assets/Scripts/PROPS/UiAudioController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class UiAudioController : MonoBehaviour
+{
+    [Header("Audio Clips")]
+    [SerializeField] private AudioClip[] varOpen;
+    [SerializeField] private AudioClip[] varClose;
+    [SerializeField] private AudioClip[] buttonClick;
+    [SerializeField] private AudioClip[] buttonSelect;
+    [SerializeField] private AudioClip[] checkBoxClic;
+
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+    }
+
+    public void PlayVarOpenSound() => PlayRandomClip(varOpen);
+    public void PlayVarCloseSound() => PlayRandomClip(varClose);
+    public void PlayButtonClickSound() => PlayRandomClip(buttonClick);
+    public void PlayButtonSelectSound() => PlayRandomClip(buttonSelect);
+
+    public void PlayCheckBoxClicSound() => PlayRandomClip(checkBoxClic);
+
+
+    private void PlayRandomClip(AudioClip[] clips)
+    {
+        if (clips == null || clips.Length == 0) return;
+
+        AudioClip clipToPlay = clips.Length == 1 ? clips[0] : clips[Random.Range(0, clips.Length)];
+
+        if (AudioManager.Instance != null && !AudioManager.Instance.IsMuted)
+        {
+            float currentSfxVolume = AudioManager.Instance.MasterVolume * AudioManager.Instance.SfxVolume;
+            audioSource.PlayOneShot(clipToPlay, currentSfxVolume);
+        }
+    }
+}
